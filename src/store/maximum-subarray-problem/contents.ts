019c3f5149ddc0c596085d5f8a63d0f4bde2462b
dozhou <dozhou@rubiconproject.com>
2018-11-20 00:42:31 -0800

    add comemnts on max subarray

--- src/store/maximum-subarray-problem/contents.ts
+++ src/store/maximum-subarray-problem/contents.ts
@@ -1,4 +1,14 @@
-export const formula = '';
-export const description = '';
+export const formula = `def kadane(A):
+  max_current = max_global = A[0]
+  for val in A[1:]:
+    max_current = max(val, val + max_current)
+    max_global = max(max_current, max_global)
+  return max_global`;
+
+export const description = `Finding the contiguous subarray within a 
+one-dimensional array of numbers which has the 
+largest sum. The list usually contains both 
+positive and negative numbers. `;
+
 export const useCases = '';
 export const example = '';
