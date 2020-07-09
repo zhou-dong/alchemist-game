e2d914ea312de34d3aed5913050497739c57e05e
Dong Zhou <dozhou@rubiconproject.com>
2020-07-09 01:01:05 -0700

    update coin change description

--- src/store/coin-change-fewest-number/contents.ts
+++ src/store/coin-change-fewest-number/contents.ts
@@ -8,8 +8,7 @@ export const formula = `if (coin > col) {
 
 export const description = `You are given coins of different denominations and a total amount 
 of money amount. Find the fewest number of coins that you need to 
-make up that amount. If that amount of money cannot be made up by 
-any combination of the coins, return -1.`;
+make up that amount.`;
 
 export const alUsecases = '';
 export const example = `
