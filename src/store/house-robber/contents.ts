cd5b3cd0d1ed36ede545b16b24c8323770af2b36
Dong Zhou <dozhou@rubiconproject.com>
2020-09-07 19:49:54 -0700

    Update contents.ts

--- src/store/house-robber/contents.ts
+++ src/store/house-robber/contents.ts
@@ -1,14 +1,29 @@
-export const formula = `if (table.length < 2) {
-  return table;
+export const formula = `class Solution {
+    
+  public int rob(int[] nums) {
+      if (nums.length == 0) {
+          return 0;
+      }
+
+      if (nums.length == 1) {
+          return nums[0];
+      }
+
+      if (nums.length == 2) {
+          return Math.max(nums[0], nums[1]);
+      }
+
+      nums[1] = Math.max(nums[0], nums[1]);
+      
+      for (int i = 2; i < nums.length; i++) {
+          nums[i] = Math.max(nums[i] + nums[i-2], nums[i-1]); 
+      }
+
+      return nums[nums.length - 1];
+  }
+
 }
-
-table[1] = Math.max(table[0], table[1]);
-
-for (let i = 2; i < table.length; i++) {
-  table[i] = Math.max(
-    table[i] + table[i - 2], table[i - 1]
-  );
-}`;
+`;
 
 export const description = `You are a professional robber planning to rob houses
 along a street. Each house has a certain amount of money stashed, 
@@ -18,18 +33,14 @@ automatically contact the police if two adjacent houses were
 broken into on the same night.
 
 Given a list of non-negative integers representing the amount 
-of money of each house, determine the maximum amount of money 
-you can rob tonight without alerting the police.`;
+of money of each house, ***determine the maximum amount of money 
+you can rob tonight without alerting the police.***`;
 
 export const alUsecases = '';
-export const example = `Input: [1,2,3,1]
-Output: 4
-Explanation: 
-    Rob house 1 (money = 1) and then rob house 3 (money = 3).
-    Total amount you can rob = 1 + 3 = 4.
-             
-Input: [2,7,9,3,1]
-Output: 12
-Explanation: 
-    Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
-    Total amount you can rob = 2 + 9 + 1 = 12.`;
+export const example = `
+- Input: \\[2, 4, 3, 1, 6, 5, 7, 6\\]
+- Output: 18
+- Explanation: 
+    + Rob house 1 (money = 2), rob house 3 (money = 3) and rob house 5 (money = 6) rob house 7 (money = 7).
+    + Total amount you can rob = 2 + 3 + 6 + 7 = 18.
+`;
