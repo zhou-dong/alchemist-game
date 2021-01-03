45b4769d134cd3f74e83ec6595a5c964952b2fc3
Dong Zhou <dozhou@rubiconproject.com>
2021-01-03 01:52:17 -0800

    Updates

---
+++ precache-manifest.772b75a50f878722440b95edc3c78bef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a54c7756c6820b7082b0e4985fac061d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24c5fe892085b0a95548",
+    "url": "/static/css/2.8117de4d.chunk.css"
+  },
+  {
+    "revision": "24c5fe892085b0a95548",
+    "url": "/static/js/2.b1a59229.chunk.js"
+  },
+  {
+    "revision": "1f6fdaab05fe898479a1032895e0157e",
+    "url": "/static/js/2.b1a59229.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91bc81ab91d886b3e789",
+    "url": "/static/js/main.def24215.chunk.js"
+  },
+  {
+    "revision": "d7a5c0a078ae040810e0",
+    "url": "/static/js/runtime-main.22d41f06.js"
+  },
+  {
+    "revision": "f6ce5696e634677f94343db90b9f66dd",
+    "url": "/static/media/Google__G__Logo.f6ce5696.svg"
+  },
+  {
+    "revision": "e50abc210803c36305475fe352047ab5",
+    "url": "/static/media/mark-github.e50abc21.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2cee6040c801f0a38fc51d1e9afa2f8b.js"
+  "/precache-manifest.772b75a50f878722440b95edc3c78bef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
